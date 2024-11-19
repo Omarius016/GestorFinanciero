c84a57b027ecc5ad64f67f1d63009d80826a21ec
Omarius016 <omarbeltran210@gmail.com>
2024-11-19 12:16:28 -0700

    Login-Hero en desarrollo, falta pasarlo a shared y detalles a afinar

---
+++ BHproyecto/src/app/pages/login/components/login-hero/login-hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-hero.component.html',
+  styleUrl: './login-hero.component.css'
+})
+export class LoginHeroComponent {
+
+}
--- BHproyecto/src/app/pages/login/login.component.ts
+++ BHproyecto/src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginHeroComponent } from './components/login-hero/login-hero.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoginHeroComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
