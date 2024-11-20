d2b6cffddfbbe35a4ac5d64a2240f1770a6578ee
Omarius016 <omarbeltran210@gmail.com>
2024-11-20 14:48:48 -0700

    Correo-input validado

--- BHproyecto/src/app/pages/login/components/login-form/login-form.component.ts
+++ BHproyecto/src/app/pages/login/components/login-form/login-form.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,ValidationErrors, AbstractControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-login-form',
   standalone: true,
@@ -14,15 +15,51 @@ export class LoginFormComponent {
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required,Validators.email, this.emailValidator()]], // Aquí aplicamos el validador personalizado
       password: ['', Validators.required]
     });
   }
+  
+  isFormValid(): boolean {
+    return this.loginForm.valid;
+  }
 
-  onSubmit(): void {
+  emailValidator() {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const email = control.value;
+      
+      const allowedDomains = [
+        'gmail.com',
+        'hotmail.com',
+        'outlook.com',
+        'yahoo.com',
+        'yahoo.es',
+        'live.com',
+        'icloud.com',
+        'uas.edu.mx'
+      ];
+
+      if (!email) return null;
+
+      const basicEmailPattern = /^[a-zA-Z0-9._-]+@/;
+      if (!basicEmailPattern.test(email)) {
+        return { invalidFormat: true };
+      }
+
+      const domain = email.split('@')[1];
+      if (!allowedDomains.includes(domain)) {
+        return { invalidDomain: true };
+      }
+      
+
+      return null;
+    };
+    
+  }
+  
+  onSubmit() {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
-      // Aquí irá la lógica de autenticación
     }
   }
 }
